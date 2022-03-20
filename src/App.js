26108df13e82c92d2ab1a27b3f4c4b78d29422e8
safagundogdu <101969719+safagundogdu@users.noreply.github.com>
2022-03-20 18:32:33 +0300

    Routes tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ComingSoon } from "./Components/ComingSoon";
-
+import {Map} from './Components/Map'
 const App = () => (
   <Routes>
+    <Route path="/hub-1" element={<Map/>}></Route>
+    <Route path="/hub-2" element={<h1>hub-2</h1>}></Route>
+    <Route path="/hub-3" element={<h1>hub-3</h1>}></Route>
+    <Route path="/hub-4" element={<h1>hub-4</h1>}></Route>
+    <Route path="/hub-5" element={<h1>hub-5</h1>}></Route>
+    <Route path="/hub-6" element={<h1>hub-6</h1>}></Route>
+    <Route path="/hub-7" element={<h1>hub-7</h1>}></Route>
+    <Route path="/hub-8" element={<h1>hub-8</h1>}></Route>
+    <Route path="/hub-9" element={<h1>hub-9</h1>}></Route>
+    <Route path="/hub-10" element={<h1>hub-10</h1>}></Route>
+    <Route path="/business-1" element={<h1>business</h1>}></Route>
+    <Route path="/business-2" element={<h1>business</h1>}></Route>
+    <Route path="/business-3" element={<h1>business</h1>}></Route>
+    <Route path="/business-4" element={<h1>business</h1>}></Route>
+    <Route path="/business-5" element={<h1>business</h1>}></Route>
+    <Route path="/business-6" element={<h1>business</h1>}></Route>
+    <Route path="/business-7" element={<h1>business</h1>}></Route>
+    <Route path="/business-8" element={<h1>business</h1>}></Route>
+    <Route path="/business-9" element={<h1>business</h1>}></Route>
+    <Route path="/business-10" element={<h1>business</h1>}></Route>
+    <Route path="/business-11" element={<h1>business</h1>}></Route>
+    <Route path="/business-12" element={<h1>business</h1>}></Route>
+    <Route path="/business-13" element={<h1>business</h1>}></Route>
+    <Route path="/business-14" element={<h1>business</h1>}></Route>
+    <Route path="/business-15" element={<h1>business</h1>}></Route>
+    <Route path="/business-16" element={<h1>business</h1>}></Route>
+    <Route path="/business-17" element={<h1>business</h1>}></Route>
+    <Route path="/business-18" element={<h1>business</h1>}></Route>
+    <Route path="/business-19" element={<h1>business</h1>}></Route>
+    <Route path="/business-20" element={<h1>business</h1>}></Route>
+    <Route path="/business-21" element={<h1>business</h1>}></Route>
+    <Route path="/business-22" element={<h1>business</h1>}></Route>
+    <Route path="/business-23" element={<h1>business</h1>}></Route>
+    <Route path="/business-24" element={<h1>business</h1>}></Route>
+    <Route path="/business-25" element={<h1>business</h1>}></Route>
+    <Route path="/business-26" element={<h1>business</h1>}></Route>
+    <Route path="/business-27" element={<h1>business</h1>}></Route>
+    <Route path="/business-28" element={<h1>business</h1>}></Route>
+    <Route path="/business-29" element={<h1>business</h1>}></Route>
+    <Route path="/business-30" element={<h1>business</h1>}></Route>
+    <Route path="/business-31" element={<h1>business</h1>}></Route>
+    <Route path="/business-32" element={<h1>business</h1>}></Route>
+    <Route path="/business-33" element={<h1>business</h1>}></Route>
+    <Route path="/business-34" element={<h1>business</h1>}></Route>
+    <Route path="/business-35" element={<h1>business</h1>}></Route>
+    <Route path="/business-36" element={<h1>business</h1>}></Route>
+    <Route path="/business-37" element={<h1>business</h1>}></Route>
+    <Route path="/business-38" element={<h1>business</h1>}></Route>
+    <Route path="/business-39" element={<h1>business</h1>}></Route>
+    <Route path="/business-40" element={<h1>business</h1>}></Route>
+    <Route path="/business-41" element={<h1>business</h1>}></Route>
+    <Route path="/business-42" element={<h1>business</h1>}></Route>
+    <Route path="/business-43" element={<h1>business</h1>}></Route>
+    <Route path="/business-44" element={<h1>business</h1>}></Route>
+    <Route path="/business-45" element={<h1>business</h1>}></Route>
+    <Route path="/business-46" element={<h1>business</h1>}></Route>
+    <Route path="/business-47" element={<h1>business</h1>}></Route>
+    <Route path="/business-48" element={<h1>business</h1>}></Route>
+    <Route path="/business-49" element={<h1>business</h1>}></Route>
+    <Route path="/business-50" element={<h1>business</h1>}></Route>
     <Route path="/" element={<ComingSoon />}></Route>
   </Routes>
 );
