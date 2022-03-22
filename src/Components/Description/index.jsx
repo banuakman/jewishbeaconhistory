4dc78ee24f5d5f6fa0d186ea4912530b30264cf7
banuakman <banu@asterya.com>
2022-03-22 17:23:11 -0400

    rename routes

--- src/Components/Description/index.jsx
+++ src/Components/Description/index.jsx
@@ -1,29 +1,35 @@
-import React,{useRef,useState} from "react";
+import React, { useRef, useState } from "react";
 import "./style.css";
 
 function Description({ header, text, hasBusiness }) {
-
   const myRef = useRef(null);
 
   const executeScroll = () => myRef.current.scrollIntoView();
 
-  const [hover,setHover] = useState(false)
-  
+  const [hover, setHover] = useState(false);
+
   return (
     <div className="description-container">
-      {
-        header !== undefined &&
-        <h1>{header}</h1>
-      }
-      {
-        hasBusiness &&
-        <button onClick={executeScroll}  onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)}><img src={hover?("location_pin_empty_black.svg"):("location_pin_empty_orange.svg")} style={{color:"#E86C4F"}}/>View Jewish businesses in this location</button>
-      }
-      <p>{text}</p>
-      {
-        hasBusiness &&
-        <span ref={myRef}>Jewish Businesses:</span>
-      }
+      {header !== undefined && <h1>{header}</h1>}
+      {hasBusiness && (
+        <button
+          onClick={executeScroll}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <img
+            src={
+              hover
+                ? "location_pin_empty_black.svg"
+                : "location_pin_empty_orange.svg"
+            }
+            style={{ color: "#E86C4F" }}
+          />
+          View Jewish businesses in this location
+        </button>
+      )}
+      <p className="description-details">{text}</p>
+      {hasBusiness && <span ref={myRef}>Jewish Businesses:</span>}
     </div>
   );
 }
--- src/Components/Map/index.jsx
+++ src/Components/Map/index.jsx
@@ -1,92 +1,125 @@
 import React from "react";
 import "./style.css";
-import { MapContainer, TileLayer, Marker } from "react-leaflet"
-import 'leaflet/dist/leaflet.css'
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import { LocationMarker } from "./LocationMarker";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-var icon = (value) => L.divIcon({
-  className: 'leaflet-div-icon',
-  html: "<div class='marker-pin'><span>"+value+"</span></div>",
-  iconSize: [30, 42],
-  iconAnchor: [15, 42]
-});
-
+var icon = (value) =>
+  L.divIcon({
+    className: "leaflet-div-icon",
+    html: "<div class='marker-pin'><span>" + value + "</span></div>",
+    iconSize: [30, 42],
+    iconAnchor: [15, 42],
+  });
 
 function Map() {
-
   let navigate = useNavigate();
 
   return (
     <div className="map-container">
-      <MapContainer style={{ height: "100%", width: "100%" }} center={[41.50492, -73.96988]} zoom={15} maxZoom={20}>
+      <MapContainer
+        style={{ height: "100%", width: "100%" }}
+        center={[41.50492, -73.96988]}
+        zoom={15}
+        maxZoom={20}
+      >
         <TileLayer
-          attribution=''
+          attribution=""
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
         />
-        <Marker position={[41.50824, -73.97739]} icon={icon(1)}
+        <Marker
+          position={[41.50824, -73.97739]}
+          icon={icon(1)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-1");
+              return navigate("/stop1");
             },
           }}
         />
-        <Marker position={[41.50818, -73.97517]} icon={icon(2)}
+        <Marker
+          position={[41.50818, -73.97517]}
+          icon={icon(2)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-2");
+              return navigate("/stop2");
             },
-          }}/>
-        <Marker position={[41.50644, -73.97276]} icon={icon(3)}
+          }}
+        />
+        <Marker
+          position={[41.50644, -73.97276]}
+          icon={icon(3)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-3");
+              return navigate("/stop3");
             },
-          }}/>
-        <Marker position={[41.50531, -73.97095]} icon={icon(4)}
+          }}
+        />
+        <Marker
+          position={[41.50531, -73.97095]}
+          icon={icon(4)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-4");
+              return navigate("/stop4");
             },
-          }}/>
-        <Marker position={[41.50492, -73.96988]} icon={icon(5)}
+          }}
+        />
+        <Marker
+          position={[41.50492, -73.96988]}
+          icon={icon(5)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-5");
+              return navigate("/stop5");
             },
-          }}/>
-        <Marker position={[41.50336, -73.96727]} icon={icon(6)}
+          }}
+        />
+        <Marker
+          position={[41.50336, -73.96727]}
+          icon={icon(6)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-6");
+              return navigate("/stop6");
             },
-          }}/>
-        <Marker position={[41.50239, -73.96602]} icon={icon(7)}
+          }}
+        />
+        <Marker
+          position={[41.50239, -73.96602]}
+          icon={icon(7)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-7");
+              return navigate("/stop7");
             },
-          }}/>
-        <Marker position={[41.50193, -73.96439]} icon={icon(8)}
+          }}
+        />
+        <Marker
+          position={[41.50193, -73.96439]}
+          icon={icon(8)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-8");
+              return navigate("/stop8");
             },
-          }}/>
-        <Marker position={[41.50269, -73.96314]} icon={icon(9)}
+          }}
+        />
+        <Marker
+          position={[41.50269, -73.96314]}
+          icon={icon(9)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-9");
+              return navigate("/stop9");
             },
-          }}/>
-        <Marker position={[41.50166, -73.9611]} icon={icon(10)}
+          }}
+        />
+        <Marker
+          position={[41.50166, -73.9611]}
+          icon={icon(10)}
           eventHandlers={{
             click: (e) => {
-              return navigate("/hub-10");
+              return navigate("/stop10");
             },
-          }}/>
-        <LocationMarker/>
+          }}
+        />
+        <LocationMarker />
       </MapContainer>
     </div>
   );
