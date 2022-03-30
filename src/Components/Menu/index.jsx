971bc19e2e47e44da6ccbd47b01dbbc8848c695d
banuakman <banu@asterya.com>
2022-03-29 21:10:14 -0400

    fix menu links

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,49 +1,96 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { isDesktop } from 'react-device-detect';
+import { isDesktop } from "react-device-detect";
 import "./style.css";
 
 function Menu() {
-
-  const [isOpen, setIsOpen] = useState(false)
-  const [isDrop,setIsDrop] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDrop, setIsDrop] = useState(false);
 
   if (isDesktop) {
     return (
       <div className="menu-container-desktop">
         <Link to="/home">Home</Link>
         <Link to="/credits">Credits</Link>
-        <button onClick={()=>{setIsDrop(!isDrop)}}>Tour Stops <img src="drop-icon.png"/></button>
-        {
-          isDrop &&
-          <div className="menu-dropdown" onMouseLeave={()=>setIsDrop(false)}>hello</div>
-        }
+        <button
+          onClick={() => {
+            setIsDrop(!isDrop);
+          }}
+        >
+          Tour Stops <img src="drop-icon.png" />
+        </button>
+        {isDrop && (
+          <div className="menu-dropdown" onMouseLeave={() => setIsDrop(false)}>
+            <div>
+              <Link to="/stop1">1 - Historic Bank Square</Link>
+            </div>
+            <div>
+              <Link to="/stop2">2 - West End of Main</Link>
+            </div>
+            <div>
+              <Link to="/stop3">3 - Main between Willow and Walnut</Link>
+            </div>
+            <div>
+              <Link to="/stop4">4 - S. Cedar and Main</Link>
+            </div>
+            <div>
+              <Link to="/stop5">5 - S. Chestnut and Main</Link>
+            </div>
+            <div>
+              <Link to="/stop6">6 - Fishkill/Teller and Main</Link>
+            </div>
+            <div>
+              <Link to="/stop7">7 - The Beacon Theatre</Link>
+            </div>
+            <div>
+              <Link to="/stop8">8 - Tioronda and Main</Link>
+            </div>
+            <div>
+              <Link to="/stop9">9 - North and Main</Link>
+            </div>
+            <div>
+              <Link to="/stop10">10 - East Main and Water</Link>
+            </div>
+          </div>
+        )}
       </div>
     );
-  }
-  else {
+  } else {
     return (
       <div className="menu-container">
-        <button className="menu-button" onClick={() => setIsOpen(true)}><img src="hamburger-icon.png" /></button>
-        {
-          isOpen &&
+        <button className="menu-button" onClick={() => setIsOpen(true)}>
+          <img src="hamburger-icon.png" />
+        </button>
+        {isOpen && (
           <div className="menu-box">
             <div className="menu-box-links">
-              <div className="menu-box-links-header"><button onClick={() => setIsOpen(false)}><img src="close-icon.png" /></button><h1>Jewish Beacon History Walk</h1></div>
+              <div className="menu-box-links-header">
+                <button onClick={() => setIsOpen(false)}>
+                  <img src="close-icon.png" />
+                </button>
+                <h1>Jewish Beacon History Walk</h1>
+              </div>
               <Link to="/">About</Link>
               <Link to="/credits">Credits</Link>
               <span>Tour Stops</span>
               <div className="menu-box-deep-links">
-                <Link to="/">#1 - Historic Bank Square</Link>
+                <Link to="/stop1">1 - Historic Bank Square</Link>
+                <Link to="/stop2">2 - West End of Main</Link>
+                <Link to="/stop3">3 - Main between Willow and Walnut</Link>
+                <Link to="/stop4">4 - S. Cedar and Main</Link>
+                <Link to="/stop5">5 - S. Chestnut and Main</Link>
+                <Link to="/stop6">6 - Fishkill/Teller and Main</Link>
+                <Link to="/stop7">7 - The Beacon Theatre</Link>
+                <Link to="/stop8">8 - Tioronda and Main</Link>
+                <Link to="/stop9">9 - North and Main</Link>
+                <Link to="/stop10">10 - East Main and Water</Link>
               </div>
-
             </div>
           </div>
-        }
+        )}
       </div>
     );
   }
-
 }
 
 export { Menu };
